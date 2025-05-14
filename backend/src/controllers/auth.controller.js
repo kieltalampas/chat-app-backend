cf5a0bf4821c783d189043c2bdd2646a40783837
Kiel <ezekieltalampas.c@gmail.com>
2025-05-14 19:34:24 +0800

    chat app initial

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+    
+}
+
+export const login = (req, res) => {
+    res.send('login up page');
+}
+
+export const logout = (req, res) => {
+    res.send('logout up page');
+}
