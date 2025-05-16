931409c24821cae6073d5d164335cf89a106fffc
Kiel <ezekieltalampas.c@gmail.com>
2025-05-16 19:42:18 +0800

    login/logout/updateProfile

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import {connectDB} from './lib/db.js';
+import cookieParser from "cookie-parser"
 
 import authRoutes from './routes/auth.route.js';
 
@@ -10,6 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
