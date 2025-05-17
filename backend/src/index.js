424f9e6c50b2f95994898089a8f3daf7525ffa69
Kiel <ezekieltalampas.c@gmail.com>
2025-05-17 20:12:22 +0800

    backend done

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import {connectDB} from './lib/db.js';
 import cookieParser from "cookie-parser"
 
 import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
 
 dotenv.config();
 const app = express();
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 
 app.listen(PORT, () => {
